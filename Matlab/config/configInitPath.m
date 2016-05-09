2a85cc883378c346d2d478c8e763a6706eeeb05c
Kacper Kosikowski <kosikowski.kacper@gmail.com>
2016-05-09 17:53:44 +0200

    Modified path config file to handle multimedia files (they're not present in the repository).

--- Matlab/config/configInitPath.m
+++ Matlab/config/configInitPath.m
@@ -1,6 +1,8 @@
+sourceDirectoriesToAdd = [{'dsp'}, {'geometry'}, {'scene'}, {'scene/room'}, {'scene/transducer'}, {'scene/transducer/receiver'}, {'scene/transducer/source'}, {'simulation'}];
+multimediaDirectoriesToAdd = [{'../../multimedia/used'}];
+
 global directoriesToAdd;
-directoriesToAdd = [{'dsp'}, {'geometry'}, {'scene'}, {'scene/room'}, {'scene/transducer'}, ...
-    {'scene/transducer/receiver'}, {'scene/transducer/source'}, {'simulation'}];
+directoriesToAdd = cat(2, sourceDirectoriesToAdd, multimediaDirectoriesToAdd);
 
 for i = 1:length(directoriesToAdd)
     addpath(directoriesToAdd{i});

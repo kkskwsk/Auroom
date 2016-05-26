13b0f586d85d1de5cbb84df7d9cc7af783f4aad0
Kacper Kosikowski <kosikowski.kacper@gmail.com>
2016-05-26 02:46:16 +0200

    Changes in configuration file - new directories added to path.

--- Matlab/config/configInitPath.m
+++ Matlab/config/configInitPath.m
@@ -1,5 +1,5 @@
-sourceDirectoriesToAdd = [{'dsp'}, {'geometry'}, {'scene'}, {'scene/room'}, {'scene/transducer'}, {'scene/transducer/receiver'}, {'scene/transducer/source'}, {'simulation'}];
-multimediaDirectoriesToAdd = [{'../../multimedia/used'}];
+sourceDirectoriesToAdd = [{'../dsp'}, {'../dsp/air'}, {'../dsp/directivity'}, {'../geometry'}, {'../scene'}, {'../scene/room'}, {'../scene/transducer'}, {'../scene/transducer/receiver'}, {'../scene/transducer/source'}, {'../simulation'}, {'../utils'}];
+multimediaDirectoriesToAdd = [{'../../../multimedia/used'}, {'../../../doc/hrtf/MIT KEMAR/full/elev0'}];
 
 global directoriesToAdd;
 directoriesToAdd = cat(2, sourceDirectoriesToAdd, multimediaDirectoriesToAdd);

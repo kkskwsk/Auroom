977dca859a67b8c542192ac7869cc9551c7625b2
Kacper Kosikowski <kosikowski.kacper@gmail.com>
2016-05-26 02:51:21 +0200

    Changes in the main script.

--- Matlab/main/main.m
+++ Matlab/main/main.m
@@ -2,21 +2,18 @@ clear all;
 clear classes;
 close all;
 
-addpath config;
+addpath ../config;
 configInitPath;
 
 simulationContext = Simulation2dContext();
-simulationContext.drawScene();
-simulationContext.showScene();
-simulationContext.processGeometry();
-simulationContext.processSounds('impulse.wav');
-simulationContext.playSynthBuf();
-simulationContext.processSounds('jeckyll.wav');
-simulationContext.playSynthBuf();
-simulationContext.processSounds('gunshot.wav');
-simulationContext.playSynthBuf();
-%simulationContext.processSounds('queen.wav');
-%simulationContext.playSynthBuf();
 
-configCleanPath;
+simulationContext.start();
+%simulationContext.drawScene();
+%simulationContext.showScene();
+
+inputSound = Sound('am.flac', 16, 0, 0);
+outputSound = simulationContext.auralize(inputSound);
+outputSound.play(0);
+
+configCleanPath;    
 rmpath config;
